645562aab1c243b05d6cd325cad32d11248bf5bd
DFranck <franckdufournet@hotmail.fr>
2024-01-17 18:07:28 +0700

    rdy to build

--- src/views/EmployeeList/EmployeeList.jsx
+++ src/views/EmployeeList/EmployeeList.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import { Table } from "../../common/components/Table/Table";
 import { useSelector } from "react-redux";
-// import { Table } from "../../../dist/pmf-Table.es";
-import "../../../dist/style.css";
 import dataMock from "../../mock/employee.json";
 export const EmployeeList = () => {
   const employees = useSelector((state) => state.createEmployee.employees);
@@ -11,7 +9,6 @@ export const EmployeeList = () => {
     <main>
       <section id="employee-div" className="container">
         <h1 className="title">Current Employees</h1>
-        {/* <Table data={employees} /> */}
         <Table data={dataMock} />
         <Link to="/">Home</Link>
       </section>
