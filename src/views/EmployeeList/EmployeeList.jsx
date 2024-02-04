88dc7d1980a3e21761bc5d8b12effd83277566de
DFranck <franckdufournet@hotmail.fr>
2024-02-04 14:47:54 +0700

    import pmf-table via link

--- src/views/EmployeeList/EmployeeList.jsx
+++ src/views/EmployeeList/EmployeeList.jsx
@@ -1,10 +1,12 @@
+import Table from "pmf-table";
 import { Link } from "react-router-dom";
+import dataMock from "../../mock/employee.json";
 export const EmployeeList = () => {
   return (
     <main>
       <section id="employee-div" className="container">
         <h1 className="title">Current Employees</h1>
-        {/* <Table data={dataMock} /> */}
+        <Table data={dataMock} />
         <Link to="/">Home</Link>
       </section>
     </main>
