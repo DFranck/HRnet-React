e9d750f931a4976a39de07068f8e2784042d231b
DFranck <franckdufournet@hotmail.fr>
2024-02-04 22:40:08 +0700

    add themeSlice

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { formInputsSlice } from "../features/CreateEmployee/formInputsSlice";
 import { createEmployeeSlice } from "../features/CreateEmployee/createEmployeeSlice";
+import { formInputsSlice } from "../features/CreateEmployee/formInputsSlice";
+import { themeSlice } from "../features/Theme/themeSlice";
 export const store = configureStore({
   reducer: {
     formInputs: formInputsSlice.reducer,
     createEmployee: createEmployeeSlice.reducer,
+    theme: themeSlice.reducer,
   },
 });
