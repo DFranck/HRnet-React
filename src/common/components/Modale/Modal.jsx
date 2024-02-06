1a72615740cd20137b0d81b86a0905d1615424b9
DFranck <franckdufournet@hotmail.fr>
2024-02-06 11:25:07 +0700

    style modale ok

--- src/common/components/Modale/Modal.jsx
+++ src/common/components/Modale/Modal.jsx
@@ -1,15 +1,15 @@
-import "./modal.css";
+import { useDispatch, useSelector } from "react-redux";
 import { setShowModal } from "../../../features/CreateEmployee/createEmployeeSlice";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import "./modal.scss";
 export const Modal = () => {
   const dispatch = useDispatch();
   const showModal = useSelector((state) => state.createEmployee.showModal);
+  const theme = useSelector((state) => state.theme.theme);
   if (showModal === true)
     return (
-      <div id="confirmation" className="modal">
+      <div id="confirmation" className={"modal" + " " + theme}>
         <div className="modal-content">
-          <p>Employee Created!</p>
+          <p className={theme}>Employee Created!</p>
           <span
             className="close-modal"
             onClick={() => dispatch(setShowModal(false))}
