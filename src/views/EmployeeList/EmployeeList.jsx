d9fa9ac28972ddc2f066b0f545218051e13cfc45
DFranck <franckdufournet@hotmail.fr>
2024-02-15 21:07:24 +0700

    normal import

--- src/views/EmployeeList/EmployeeList.jsx
+++ src/views/EmployeeList/EmployeeList.jsx
@@ -1,4 +1,4 @@
-import Table from "pmf-table";
+import { Table } from "pmf-table";
 import { useSelector } from "react-redux";
 import { Header } from "../../common/components/Header/Header.jsx";
 import { Nav } from "../../common/components/Nav/Nav.jsx";
