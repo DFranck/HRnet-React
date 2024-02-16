daed49b4360c08ed94d1e5cfedb707bdb482e25c
DFranck <franckdufournet@hotmail.fr>
2024-02-17 01:11:37 +0700

    utilisation de pmf-table le package npm

--- src/views/EmployeeList/EmployeeList.jsx
+++ src/views/EmployeeList/EmployeeList.jsx
@@ -1,9 +1,11 @@
 import { Table } from "pmf-table";
+import "pmf-table/style.css";
 import { useSelector } from "react-redux";
 import { Header } from "../../common/components/Header/Header.jsx";
 import { Nav } from "../../common/components/Nav/Nav.jsx";
 // import dataMock from "../../mock/employee.json";
 import "./table.scss";
+
 export const EmployeeList = () => {
   const theme = useSelector((state) => state.theme.theme);
   const data = useSelector((state) => state.createEmployee.employees);
