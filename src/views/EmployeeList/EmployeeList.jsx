18206a26eedec47e75d01cea1a59df41a2609570
DFranck <franckdufournet@hotmail.fr>
2024-02-20 10:27:24 +0700

    fusion mockedData + new data

--- src/views/EmployeeList/EmployeeList.jsx
+++ src/views/EmployeeList/EmployeeList.jsx
@@ -3,19 +3,19 @@ import "pmf-table/style.css";
 import { useSelector } from "react-redux";
 import { Header } from "../../common/components/Header/Header.jsx";
 import { Nav } from "../../common/components/Nav/Nav.jsx";
-// import dataMock from "../../mock/employee.json";
+import dataMock from "../../mock/employee.json";
 import "./table.scss";
 
 export const EmployeeList = () => {
   const theme = useSelector((state) => state.theme.theme);
   const data = useSelector((state) => state.createEmployee.employees);
+  const displayData = [...data, ...dataMock];
   return (
     <main className={theme}>
       <section id="employee-div" className="container">
         <Header />
         <Nav />
-        {/* <Table data={dataMock} /> */}
-        <Table data={data} />
+        <Table data={displayData} />
       </section>
     </main>
   );
