a2f2c152c984d0e0fbdadb1f7f23821b5bb4a284
DFranck <franckdufournet@hotmail.fr>
2024-02-20 10:34:24 +0700

    rename dautre fichier scss

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux";
 import { AppRouter } from "../router/AppRouter";
 import "../styles/global.scss";
-// import "./app.css";
 import { store } from "./store";
 function App() {
   return (
